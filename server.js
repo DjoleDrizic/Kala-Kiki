adaa0e1bb627630a356ae84381019b85422768f7
Vuk Radonjic <radonjic.vuk02@gmail.com>
2022-03-31 13:51:44 +0200

    Staticni files

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
 
+const options = {
+    extensions: ['htm', 'html'],
+    index: true
+}
+app.use(express.static('public', options))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/api/users', require('./routes/api/users'))
+
+
 app.get('/', (req, res) => {
     res.send('Online')
     console.log("Online");
